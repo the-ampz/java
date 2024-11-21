be6b3ddb50ed1d28e2376d72efb26200be68c951
Guilherme-Dias-gomes <guilhermediasgomes2@gmail.com>
2024-11-20 21:35:29 -0300

    adicionando models

---
+++ src/main/java/br/com/fiap/energysaving/EnergySavingApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.energysaving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergySavingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnergySavingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/energysaving/EnergySavingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.energysaving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergySavingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
